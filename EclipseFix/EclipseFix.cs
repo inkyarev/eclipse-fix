5b630677697a7de8cecd39f3bb1852ad1daa3996
Rev <tustora@gmail.com>
2024-12-23 21:18:53 +0500

    Initial commit

---
+++ EclipseFix/EclipseFix.cs
@@ -0,0 +1,50 @@
+using BepInEx;
+using RoR2;
+using UnityEngine;
+using HealthComponent = On.RoR2.HealthComponent;
+
+namespace EclipseFix;
+
+[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+public class ExamplePlugin : BaseUnityPlugin
+{
+    private const string PluginGUID = PluginAuthor + "." + PluginName;
+    private const string PluginAuthor = "InkyaRev";
+    private const string PluginName = "EclipseFixPlugin";
+    private const string PluginVersion = "1.0.0";
+    
+    public void Awake()
+    {
+        Log.Init(Logger);
+        HealthComponent.RepeatHealComponent.FixedUpdate += (_, selfMonoBehaviour) =>
+        {
+            if (selfMonoBehaviour is RoR2.HealthComponent.RepeatHealComponent self)
+            {
+                self.timer -= Time.fixedDeltaTime;
+                if (self.timer > 0.0)
+                    return;
+                self.timer = 0.2f;
+                if (self.reserve <= 0.0)
+                    return;
+                var amount = Mathf.Min((float) (self.healthComponent.fullHealth * (double) self.healthFractionToRestorePerSecond * 0.2), self.reserve);
+                self.reserve -= amount;
+                var procChainMask = new ProcChainMask();
+                procChainMask.AddProc(ProcType.RepeatHeal);
+                if (self.healthComponent.body.teamComponent.teamIndex == TeamIndex.Player && Run.instance.selectedDifficulty >= DifficultyIndex.Eclipse5)
+                {
+                    amount *= 2f;
+                }
+                self.healthComponent.Heal(amount, procChainMask);
+            }
+            else
+            {
+                Log.Error("WHat the fuck");
+            }
+        };
+        On.RoR2.EclipseRun.OverrideRuleChoices += (orig, self, include, exclude, seed) =>
+        {
+            orig(self, include, exclude, seed);
+            self.ForceChoice(include, exclude, $"Items.{RoR2Content.Items.ShieldOnly.name}.Off");
+        };
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Eclipse Fix
+
+## Whatawhy
+This is a mod for RoR2 that makes Corpsebloom an actual item on Eclipse 5+, it also disables Transcendence from eclipse runs so you can no longer cheese every eclipse level with 1 item.
+
+I made it 'cause i'm a Corpsebloom enjoyer and also dislike how 1 item breaks the entire gamemode. (Also because [LunarRework](https://thunderstore.io/package/Prototype/LunarRework/) doesn't have configuration)
+
+## Attribution
+This mod ports some of the features of [LunarRework](https://thunderstore.io/package/Prototype/LunarRework/) as a standalone mod. Go check it out!
